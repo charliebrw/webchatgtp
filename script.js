17866516ad477e5cc29bf7c743a9383d62db16a1
charliebrw <carlososktegui@gmail.com>
2024-03-13 20:58:24 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Animación de scroll suave al hacer clic en enlaces de la barra de navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
